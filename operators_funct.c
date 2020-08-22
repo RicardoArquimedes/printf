4bed6ae6c76111c9ec2f4bb55d32037433972512
RicardoArquimedes <richardcqt28@gmail.com>
2020-08-22 00:27:55 +0000

    add binary

--- operators_funct.c
+++ operators_funct.c
@@ -14,15 +14,18 @@ int (*operators_funct(const char oper))()
 		{"s", print_string},
 		{"d", print_integer},
 		{"i", print_integer},
-		{NULL, NULL}
+		{"b", print_bin},
+		{NULL, NULL},
 	};
 	unsigned int x = 0;
 
 	while (op_f[x].oper)
 	{
-		if (op_f[x].oper[0] == oper)
+		if (op_f[x].oper[0] == oper) /* En que iteracion encontro la operacion 
+		                                se encuentra una s una i, .oper accede que si es 
+						igua es decir que si d es = d 0 s es =s*/
 		{
-			return (op_f[x].f);
+			return (op_f[x].f); /*aqui retorn op_f = es aqui f= a la funcion print int, */
 		}
 		x++;
 	}
